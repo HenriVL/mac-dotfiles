619e5d0356fc95627bc6feaad207e160d122ccd0
HenriVL <77745408+HenriVL@users.noreply.github.com>
2022-08-10 13:40:05 +0200

    Update xmonad.hs

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -2,25 +2,33 @@ import XMonad
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Util.NamedScratchpad
 
 import XMonad.Config.Azerty
+import XMonad.StackSet as W
 
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.WindowArranger
+import XMonad.Layout.Fullscreen
 
 import XMonad.Hooks.EwmhDesktops
+import XMonad.ManageHook
 
 import XMonad.Actions.MouseResize
+import qualified XMonad.Actions.FlexibleManipulate as Flex
 
-myLayout = gaps[(L,80)] $ tiled ||| Mirror tiled ||| Full
+mySpacing :: Int
+mySpacing = 10
+
+myLayout = toggleLayouts (Full) (tiled ||| Mirror tiled)
   where
     tiled   = Tall nmaster delta ratio
-    nmaster = 1      -- Default number of windows in the master pane
-    ratio   = 1/2    -- Default proportion of screen occupied by master pane
-    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+    nmaster = 1     
+    ratio   = 1/2  
+    delta   = 3/100
 
 myKeys :: [(String, X ())]
 myKeys = [
@@ -28,17 +36,38 @@ myKeys = [
      , ("M-C-k", incWindowSpacing 4)
      , ("M-C-h", decScreenSpacing 4)
      , ("M-C-l", incScreenSpacing 4)
-     , ("M-S-a", sendMessage Arrange)
      , ("M-S-h", sendMessage (IncreaseDown 1))
      , ("M-S-l", sendMessage (IncreaseUp 1))
+     , ("M-S-f", do 
+                 sendMessage (Toggle "Full")
+                 toggleScreenSpacingEnabled
+                 toggleWindowSpacingEnabled) 
+     , ("M-f", do 
+               sendMessage (Toggle "Full")
+               setScreenSpacingEnabled True
+               setWindowSpacingEnabled True)
+     , ("M-b", namedScratchpadAction scratchpads "htop")
      ]
+
+myMouse :: [((ButtonMask, Button), Window -> X ())]
+myMouse = [
+        ]
+
+scratchpads :: [NamedScratchpad]
+scratchpads = [
+    NS "htop" "kitty -e htop" (title =? "htop") defaultFloating
+    ] where role = stringProperty "WM_WINDOW_ROLE"
+
+
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ belgianConfig
     { modMask    = mod4Mask
     , layoutHook = mouseResize $ windowArrange $ spacingWithEdge 10 $ smartBorders $ myLayout
     , terminal   = "kitty"
     , startupHook = do
-            spawn "~/.config/scripts/startup.sh"
-    , focusedBorderColor = "#8BE9FD"
+          spawn "~/.config/scripts/startup.sh"
     , normalBorderColor  = "#262834"
+    , focusedBorderColor = "#6C7D9B"
+    , manageHook = namedScratchpadManageHook scratchpads
     } `additionalKeysP` myKeys
+      `additionalMouseBindings` myMouse
